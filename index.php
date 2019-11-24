0600eeda961b5841de5c6a61c60b57c097f3c9cf
Josué Francisco <josuefrancisco2017@outlook.pt>
2019-11-24 18:50:17 +0100

    verify cpf

--- index.php
+++ index.php
@@ -1,16 +1,12 @@
 <?php
 
-require_once __DIR__ . "/class/atributos-metodos.php";
+require_once __DIR__ . "/class/metodos-estaticos.php";
 
-$c = new Carro();
+$cpf = new Doc();
 
-$c->setModelo("Ford");
+$cpf->setNumero("87988389033");
 
-$c->setMotor("1.6");
-
-$c->setAno("2000");
-
-var_dump($c->exibir());
+var_dump($cpf->getNumero());
 
 
 ?>
